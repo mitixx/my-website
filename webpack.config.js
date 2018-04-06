a08dc05580ad6d28bd4316884417606fc9ad5453
未知 <haruokame05@yahoo.co.jp>
2018-04-06 19:28:19 +0900

    add:webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 const config = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './main.js',
+  context: path.resolve(__dirname),
+  entry: 'main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js',
   },
   devServer: {
-    port: 3000
+    port: 3000,
+    open: true
   },
 };
 
-module.exports = config;
+module.exports = {
+    mode:'development',
+};
