804187d2f4119d2b437d4305487a0e61a97664b3
未知 <haruokame05@yahoo.co.jp>
2018-04-09 03:48:41 +0900

    add:webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 const config = {
-  context: path.resolve(__dirname),
+  context: path.resolve(__dirname,'/'),
   entry: 'main.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname,'/'),
     filename: 'bundle.js',
   },
   devServer: {
+    contentBase: path.join(__dirname,'/'),
+    watchContentBase: true,
     port: 3000,
-    open: true
+    open: true,
+    hot:true
   },
 };
 
